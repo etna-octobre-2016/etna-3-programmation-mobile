f80bc874f01bc05658668597a2b0f5ab62ae2498
Itadakimas <said.ahemt@gmail.com>
2016-07-08 17:29:19 +0200

    adds my account sidebar button

--- app/src/main/java/dev/etna/jabberclient/MainActivity.java
+++ app/src/main/java/dev/etna/jabberclient/MainActivity.java
@@ -3,6 +3,7 @@ package dev.etna.jabberclient;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -92,29 +93,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera)
+        if (id == R.id.sidenav_account)
         {
-            // Handle the camera action
-        }
-        else if (id == R.id.nav_gallery)
-        {
-
-        }
-        else if (id == R.id.nav_slideshow)
-        {
-
-        }
-        else if (id == R.id.nav_manage)
-        {
-
-        }
-        else if (id == R.id.nav_share)
-        {
-
-        }
-        else if (id == R.id.nav_send)
-        {
-
+            Log.i("SIDENAV", "click on my account button");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
