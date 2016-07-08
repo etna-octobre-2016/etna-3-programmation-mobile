6a366062ca80cb4786eec3690e77099dabd89608
Itadakimas <said.ahemt@gmail.com>
2016-07-08 18:27:06 +0200

    replaces settings button with logout button

--- app/src/main/java/dev/etna/jabberclient/MainActivity.java
+++ app/src/main/java/dev/etna/jabberclient/MainActivity.java
@@ -67,8 +67,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
+        if (id == R.id.action_logout)
         {
+            Log.d("OPTIONS", "logout");
             return true;
         }
 
