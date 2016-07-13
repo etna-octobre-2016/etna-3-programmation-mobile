8f116e9b23e203a6882fb827dbfa4577dee00608
Itadakimas <said.ahemt@gmail.com>
2016-07-13 14:18:05 +0200

    implements login unit tests

---
+++ app/src/androidTest/java/dev/etna/jabberclient/UserLoginApplicationTest.java
@@ -0,0 +1,44 @@
+package dev.etna.jabberclient;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import dev.etna.jabberclient.xmpp.XMPPService;
+import dev.etna.jabberclient.xmpp.XMPPServiceException;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class UserLoginApplicationTest
+{
+    private XMPPService service;
+
+    @Test
+    public void testLogin() throws Exception
+    {
+        this.service = new XMPPService("johndoe000001", "Password1", "jabber.hot-chilli.net");
+        this.service.connect();
+        this.service.login();
+        this.service.logout();
+        assertTrue("Should be logged", true);
+    }
+
+    @Test
+    public void testLoginWithBadCredentials() throws Exception
+    {
+        try
+        {
+            this.service = new XMPPService("johndoe000001", "Password", "jabber.hot-chilli.net");
+            this.service.connect();
+            this.service.login();
+            this.service.logout();
+            assertTrue("Should not be logged", false);
+        }
+        catch (XMPPServiceException e)
+        {
+            assertTrue("Should not be logged", true);
+        }
+    }
+}
